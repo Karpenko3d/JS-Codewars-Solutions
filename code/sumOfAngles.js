2a5c59fe215a44ff1f5fd69d9164616922bc6ea9
karpenko3d <karpenko3d@gmail.com>
2020-11-03 23:00:41 -0800

    new solutions

--- code/sumOfAngles.js
+++ code/sumOfAngles.js
@@ -1,11 +1,19 @@
-//! ⭐ PROBLEM
+// ⭐ PROBLEM: https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/javascript
 /* 
 Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
-https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/javascript
+HINT: formula 180 * (n - 2)
 */
 
 function angle(n) {
 	return (n - 2) * 180
+}
+
+console.log(angle(3)) //? 180
+console.log(angle(4)) //? 360
+
+// OPTION-2
+function angle_2(n) {
+	return 180 * (n - 2)
 }
 
 console.log(angle(3)) //? 180
---
+++ code/thirdAngleOfTriangle.js
@@ -0,0 +1,15 @@
+// ⭐ PROBLEM: https://www.codewars.com/kata/5a023c426975981341000014/train/javascript 
+/* 
+You are given two interior angles (in degrees) of a triangle. Write a function to return the 3rd.
+Note: only positive integers will be tested. https://en.wikipedia.org/wiki/Triangle
+HINT: formula a + b + c = 180
+*/
+
+function otherAngle(a, b) {
+	return 180 - a - b
+}
+
+console.log(otherAngle(30, 60)) //? 90
+console.log(otherAngle(60, 60)) //? 60
+console.log(otherAngle(43, 78)) //? 59
+console.log(otherAngle(10, 20)) //? 150
